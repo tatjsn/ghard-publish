d9e40a767ecc58fa0a0ea63109e37cb527b75370
Taz Jackson <tatjsn@users.noreply.github.com>
2025-09-20 22:36:08 +0100

    Add line integration

--- api/cron.py
+++ api/cron.py
@@ -1,11 +1,28 @@
 from http.server import BaseHTTPRequestHandler
- 
+import os
+import linebot.v3.messaging
+
+configuration = linebot.v3.messaging.Configuration(
+    access_token = os.environ['LINE_TOKEN']
+)
+
+def line_push_message(message):
+    with linebot.v3.messaging.ApiClient(configuration) as api_client:
+        api_instance = linebot.v3.messaging.MessagingApi(api_client)
+        push_message_request = linebot.v3.messaging.PushMessageRequest(
+            to=os.environ.get('LINE_USER_ID'),
+            messages=[linebot.v3.messaging.TextMessage(text=message)]
+        )
+        api_instance.push_message(push_message_request)
+
 class handler(BaseHTTPRequestHandler):
- 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
-        print("Triggered")
+        line_push_message('Hello from handler')
         return
+
+if __name__ == '__main__':
+    line_push_message('Hello from main')
